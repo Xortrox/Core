7f9a90375be3606e793081337736103a650f4f81
KKonaOG <kkonaog@fanet.dev>
2025-05-05 23:49:21 -0500

    New Core

---
+++ dist/index.js
@@ -0,0 +1,92 @@
+// src/core/pluginLoader.ts
+class PluginLoader {
+  plugins = [];
+  constructor() {
+    this.plugins = [];
+    document.highlite.plugins = {};
+  }
+  async registerPlugin(pluginClass) {
+    const pluginInstance = new pluginClass;
+    await pluginInstance.init();
+    document.highlite.plugins[pluginInstance.pluginName] = pluginInstance;
+    this.plugins.push(pluginInstance);
+    return true;
+  }
+  async startAll() {
+    for (const plugin of this.plugins) {
+      plugin.start();
+    }
+  }
+  async stopAll() {
+    for (const plugin of this.plugins) {
+      plugin.stop();
+    }
+  }
+  async postInitAll() {
+    for (const plugin of this.plugins) {
+      if (plugin.postInit) {
+        plugin.postInit();
+      }
+    }
+  }
+}
+
+// src/core/core.ts
+class Highlite {
+  pluginLoader = new PluginLoader;
+  constructor() {
+    console.log("Highlite Core Initializing!");
+  }
+  start() {
+    console.log("Highlite Core Started!");
+    this.pluginLoader.postInitAll();
+    this.pluginLoader.startAll();
+  }
+  stop() {
+    console.log("Highlite Core Stopped!");
+    this.pluginLoader.stopAll();
+  }
+  reload() {
+    console.log("Highlite Core Reloading");
+    this.stop();
+    this.start();
+  }
+  registerDataInstance() {}
+  registerFunctionHook() {}
+}
+
+// src/core/interfaces/plugin.class.ts
+class Plugin {
+  log(...args) {
+    console.log(`[${this.pluginName}]`, args);
+  }
+  info(...args) {
+    console.info(`[${this.pluginName}]`, args);
+  }
+  warn(...args) {
+    console.warn(`[${this.pluginName}]`, args);
+  }
+  error(...args) {
+    console.error(`[${this.pluginName}]`, args);
+  }
+}
+
+// src/core/plugins/RememberMe.ts
+class RememberMe extends Plugin {
+  pluginName = "RememberMe";
+  async init() {
+    this.warn("Init Reached");
+  }
+  async start() {
+    this.warn("Start Reached");
+  }
+  async stop() {
+    this.warn("Stop Reached");
+    return;
+  }
+}
+
+// src/index.ts
+document.highlite = new Highlite;
+document.highlite.pluginLoader.registerPlugin(RememberMe);
+document.highlite.start();
